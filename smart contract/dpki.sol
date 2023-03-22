a041f087646a22ba68b53e0e37f425b01f3e7aa2
AlexFalzone <falzonealex@protonmail.com>
2023-03-22 17:13:50 +0100

    added revoke, update and get

--- smart contract/dpki.sol
+++ smart contract/dpki.sol
@@ -41,8 +41,7 @@ contract DomainRegistry {
 
         // Verifica che il dominio padre sia registrato e che l'owner sia lo stesso
         require(
-            domains[parentDomain].owner == msg.sender,
-            "Parent domain not registered or not owned by sender"
+            domains[parentDomain].owner == msg.sender,"Parent domain not registered or not owned by sender"
         );
 
         // Registra il dominio
@@ -54,7 +53,7 @@ contract DomainRegistry {
 
             //se la data è 0, allora la data è il massimo valore di uint256
             //altrimenti la data è quella passata come parametro
-            data: data == 0 ? type(uint256).max : data
+            data: data == 0 ? type(uint256).max : block.timestamp + data
         });
 
         emit DomainCreated(hostname, ip, certificate, data, msg.sender);
@@ -73,6 +72,46 @@ contract DomainRegistry {
             parentDomainBytes[i] = hostnameBytes[lastDot + 1 + i];
         }
         return string(parentDomainBytes);
+    }
+
+    event DomainUpdated(
+        string hostname,
+        string ip,
+        string certificate,
+        uint256 data
+    );
+
+    function updateDomain(
+        string calldata hostname,
+        string calldata ip,
+        string calldata certificate,
+        uint256 data
+    ) external {
+        require(domains[hostname].owner == msg.sender, "Domain not owned by sender");
+
+        domains[hostname].ip = ip;
+        domains[hostname].certificate = certificate;
+        domains[hostname].data = data == 0 ? type(uint256).max : block.timestamp + data;
+
+        emit DomainUpdated(hostname, ip, certificate, data);
+    }
+
+    event DomainDeleted(string hostname);
+    function deleteDomain(string calldata hostname) external {
+        require(domains[hostname].owner == msg.sender, "Domain not owned by sender");
+
+        delete domains[hostname];
+
+        emit DomainDeleted(hostname);
+    }
+
+    function getCertificate(string calldata hostname) external view returns (string memory) {
+        Domain storage domain = domains[hostname];
+
+        require(bytes(domain.hostname).length > 0, "Domain not found");
+        require(domain.data > block.timestamp, "Certificate has expired");
+
+        return domain.certificate;
     }
 }
 
