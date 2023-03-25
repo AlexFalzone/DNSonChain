914acacfb979091f16b801da402307500a061058
AlexFalzone <falzonealex@protonmail.com>
2023-03-25 11:58:27 +0100

    refactoring

--- dns/dns.go
+++ dns/dns.go
@@ -5,11 +5,8 @@ import (
 	"net"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/miekg/dns"
-)
-
-const (
-	dnsServer = "1.1.1.1:53"
 )
 
 /*
@@ -19,69 +16,74 @@ Leggenda variabile ex:
 3 = tipo non A
 */
 
+const (
+	dnsServer = "1.1.1.1:53"
+
+	TypeASomet    = 1
+	TypeANonSomet = 2
+	TypeNonA      = 3
+)
+
+// Forward the request to the DNS server and send the response back to the browser
+func forwardRequest(conn net.PacketConn, msg *dns.Msg, addr net.Addr) {
+	fwd, err := dns.Exchange(msg, dnsServer)
+	if err != nil {
+		log.Error("Error forwarding DNS request", "err", err)
+		return
+	}
+
+	// Send the response back to the browser
+	packageBytes, _ := fwd.Pack()
+	_, err = conn.WriteTo(packageBytes, addr)
+	if err != nil {
+		log.Error("Error writing DNS response", "err", err)
+		return
+	}
+}
+
+// Extract the hostname from the DNS request
+func extractHostnameFromDNS(msg *dns.Msg) string {
+	hostname := msg.Question[0].Name
+	return strings.TrimSuffix(hostname, ".")
+}
+
+// Check if the DNS request is of type A or AAAA
+func isTypeAQuery(msg *dns.Msg) bool {
+	return len(msg.Question) > 0 && (msg.Question[0].Qtype == dns.TypeA || msg.Question[0].Qtype == dns.TypeAAAA)
+}
+
 func HandleRequest(conn net.PacketConn) (hostname string, ex int8) {
-	// Buffer per leggere la richiesta DNS
 	buf := make([]byte, 1024)
 
-	// Legge la richiesta dal conn
+	// Read the request from the browser
 	n, addr, err := conn.ReadFrom(buf)
 	if err != nil {
 		fmt.Println("Error reading from connection:", err)
 		return
 	}
 
-	// Crea un messaggio DNS dal buffer
+	// Create a new DNS message from the request
 	msg := &dns.Msg{}
 	if err := msg.Unpack(buf[:n]); err != nil {
 		fmt.Println("Error unpackaging DNS message:", err)
-		return hostname, 3
+		return hostname, TypeNonA
 	}
 
-	// Verifica se la richiesta è di tipo A
-	if len(msg.Question) == 0 || (msg.Question[0].Qtype != dns.TypeA && msg.Question[0].Qtype != dns.TypeAAAA) {
-		// inoltra la richiesta al server DNS cloudflare
-		fwd, err := dns.Exchange(msg, dnsServer)
-		if err != nil {
-			fmt.Println("Error forwarding DNS request:", err)
-			return
-		}
-		// Scrive la risposta sul conn
-		packageBytes, _ := fwd.Pack()
-		_, err = conn.WriteTo(packageBytes, addr)
-		if err != nil {
-			fmt.Println("Error writing DNS response:", err)
-			return
-		}
-		return
+	if !isTypeAQuery(msg) {
+		forwardRequest(conn, msg, addr)
+		return hostname, TypeNonA
 	}
 
-	// Estrae il nome dell'host dalla richiesta
-	hostnameFromDNS := msg.Question[0].Name
+	hostnameFromDNS := extractHostnameFromDNS(msg)
 
-	hostnameFromDNS = strings.TrimSuffix(hostnameFromDNS, ".")
-
-	//se la richiesta NON ha un suffisso .bit allora inoltra la richiesta al server DNS cloudflare
+	// if the request does NOT have a .bit suffix then forward the request to the cloudflare DNS server
 	if !(strings.HasSuffix(hostnameFromDNS, ".somet")) {
 		fmt.Println("DNS esterno:", hostnameFromDNS)
 
-		// inoltra la richiesta al server DNS cloudflare
-		fwd, err := dns.Exchange(msg, dnsServer)
-		if err != nil {
-			fmt.Println("Error forwarding DNS request:", err)
-			return
-		}
-
-		// Send the response back to the browser
-		packageBytes, _ := fwd.Pack()
-		_, err = conn.WriteTo(packageBytes, addr)
-		if err != nil {
-			fmt.Println("Error writing DNS response:", err)
-			return
-		}
-		return hostnameFromDNS, 2
+		forwardRequest(conn, msg, addr)
+		return hostnameFromDNS, TypeANonSomet
 	}
-	fmt.Println("DNS interno:", hostnameFromDNS)
 
-	//hostname con il prefisso .somet
-	return hostnameFromDNS, 1
+	fmt.Println("DNS interno:", hostnameFromDNS)
+	return hostnameFromDNS, TypeASomet
 }
