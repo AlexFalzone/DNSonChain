f894b068f97a307fee9673d8a4d42855bdad1f3b
AlexFalzone <falzonealex@protonmail.com>
2023-04-10 18:32:07 +0200

    improvement

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"runtime"
+	"sync"
 	"test/dns"
 	"test/inject"
 	"test/request"
@@ -16,15 +17,10 @@ TO-DO:
 */
 
 func main() {
-	// Create a new connection UDP to port 53
-	conn, err := net.ListenPacket("udp", ":53")
-	if err != nil {
-		fmt.Println("Error listening on DNS port:", err)
-		return
-	}
-	defer conn.Close()
 
 	var url string
+	var cert string
+	var errRequest error
 
 	for {
 		/*
@@ -58,43 +54,69 @@ func main() {
 			}
 		} else if choiceInt == 1 || choiceInt == 2 { //infura or localhost from the main menu
 
-			hostname, _ := dns.HandleRequest(conn)
-
-			if choiceInt == 1 { //Infura
-				url = "https://sepolia.infura.io/v3/d777809793694d9dacf5e1f94bfec65a"
-			} else if choiceInt == 2 { //Localhost
-				url = "http://localhost:8545"
-			}
-			client, err := request.DialClient(url)
+			// Create a new connection UDP to port 53
+			conn, err := net.ListenPacket("udp", ":5354")
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("Error listening on DNS port:", err)
+				return
 			}
+			defer conn.Close()
 
-			// Get certificate
-			_, certBytes, err := request.Request(client, 4, hostname, "")
-			if err != nil {
-				fmt.Println(err)
-			}
+			for {
+				hostname, ex := dns.HandleRequest(conn)
 
-			// Convert certificate to PEM
-			certString, err := util.ConvertCertToPEM(certBytes)
-			if err != nil {
-				fmt.Println(err)
-			}
+				if ex == dns.TypeASomet {
 
-			//INJECT CERTIFICATE
-			switch runtime.GOOS {
-			case "windows":
-				fmt.Println("Windows")
-			case "linux":
-				fmt.Println("Linux")
+					if choiceInt == 1 { //Infura
+						url = "https://sepolia.infura.io/v3/d777809793694d9dacf5e1f94bfec65a"
+					} else if choiceInt == 2 { //Localhost
+						url = "http://localhost:8545"
+					}
 
-				inject.InjcetLinux(certString, hostname)
-				if err != nil {
-					fmt.Println(err)
+					client, err := request.DialClient(url)
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					var wg sync.WaitGroup
+					wg.Add(1)
+					// Get certificate
+					go func() {
+						defer wg.Done()
+						_, cert, errRequest = request.Request(client, 4, hostname, "")
+						if errRequest != nil {
+							fmt.Println(err)
+						}
+					}()
+
+					wg.Wait()
+
+					if errRequest == nil {
+						// Remove the first character of the certificate (it's a "0")
+						// because the blockchain returns the certificate with a "0" at the beginning
+						cert = cert[1:]
+						fmt.Println("Richiesta ricevuta:", cert)
+					}
+
+					//INJECT CERTIFICATE
+					switch runtime.GOOS {
+					case "windows":
+						fmt.Println("Windows")
+					case "linux":
+						fmt.Println("Linux")
+
+						certpath, err := inject.SaveCertToFile(cert)
+						if err != nil {
+							fmt.Println(err)
+						}
+
+						err = inject.InjcetLinux(hostname, certpath)
+						if err != nil {
+							fmt.Println(err)
+						}
+					}
 				}
 			}
 		}
 	}
-
 }
