0fd550342a8502ba19e63ea34130017822af889d
AlexFalzone <falzonealex@protonmail.com>
2023-04-16 14:56:11 +0200

    fixed estimated gas, added comment and added a date for the certificate in the blockchain

--- request/certificatesManagement.go
+++ request/certificatesManagement.go
@@ -6,17 +6,28 @@ import (
 	"test/util"
 )
 
+// CertificatesManagement is a helper function for managing certificates.
+// It takes an integer representing the desired action (1: generate, 2: revoke, 3: update),
+// and the Ethereum node URL as input. Based on the action, it calls the appropriate function
+// in the domain registry contract.
+//
+// Parameters:
+//   - choice: Integer representing the desired action (1: generate, 2: revoke, 3: update).
+//   - url: The Ethereum node URL as a string.
 func CertificatesManagement(choice int, url string) {
 	var name string
+	var expirationDate string
 
 	switch choice {
 	case 1, 3: // Generate or Update certificate
 		fmt.Println("Insert the name of the certificate to update:")
+		fmt.Scan(&name)
+		fmt.Println("Insert the expiration date in the format yyyy-mm-dd:")
+		fmt.Scan(&expirationDate)
 	case 2: // Revoke certificate
 		fmt.Println("Insert the name of the certificate to revoke:")
+		fmt.Scan(&name)
 	}
-
-	fmt.Scan(&name)
 
 	client, err := DialClient(url)
 	if err != nil {
@@ -36,7 +47,13 @@ func CertificatesManagement(choice int, url string) {
 		return
 	}
 
-	_, _, err = Request(client, choice, name, certPEM)
+	expirationTimestamp, err := util.ParseExpirationDate(expirationDate)
+	if err != nil {
+		fmt.Println("Error parsing expiration date:", err)
+		return
+	}
+
+	_, _, err = Request(client, choice, name, certPEM, expirationTimestamp)
 	if err != nil {
 		fmt.Println(err)
 		return
--- request/request.go
+++ request/request.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"strings"
 	"sync"
 	"test/domainRegistry"
 	"test/util"
-	"time"
 
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -37,7 +39,15 @@ const (
 	contractAdress = "0x987a844139EC14B97e91E9e524dc86A24DC8D8bF"
 )
 
-// DialClient returns a client to interact with the blockchain
+// DialClient returns a singleton client instance to interact with the Ethereum blockchain.
+// If the client is not yet initialized, it tries to establish a connection to the specified URL.
+// It returns an error if the connection fails.
+// Parameters:
+//   - url: The Ethereum node URL as a string.
+//
+// Returns:
+//   - *ethclient.Client: The client instance to interact with the Ethereum blockchain.
+//   - error: An error, if any occurs during the connection process.
 func DialClient(url string) (*ethclient.Client, error) {
 	once.Do(func() {
 		client, err = ethclient.Dial(url)
@@ -50,30 +60,50 @@ func DialClient(url string) (*ethclient.Client, error) {
 	}
 }
 
-func createDomain(auth *bind.TransactOpts, instance *domainRegistry.DomainRegistry, domain string, certificate string) {
-	//FOR NOW
-	//insert a 1 day validity
-	date := time.Now().Add(time.Duration(time.Now().Day())).Unix()
+// createDomain creates a new domain in the domain registry contract.
+// It takes the transaction options, the contract instance, the domain name,
+// and the certificate string as input. It sends the transaction and logs the result.
+//
+// Parameters:
+//   - auth: *bind.TransactOpts representing the transaction options.
+//   - instance: *domainRegistry.DomainRegistry instance representing the contract.
+//   - domain: The domain name as a string.
+//   - certificate: The certificate as a PEM-encoded string.
+func createDomain(auth *bind.TransactOpts, instance *domainRegistry.DomainRegistry, domain string, certificate string, expirationTimestamp int64) {
 
-	tx, err := instance.CreateDomain(auth, domain, util.GetIP(), certificate, big.NewInt(date))
+	tx, err := instance.CreateDomain(auth, domain, util.GetIP(), certificate, big.NewInt(expirationTimestamp))
 	if err != nil {
 		log.Fatalf("Impossibile creare il dominio: %v", err)
 	}
 	fmt.Printf("Domain created: %s", tx.Hash().Hex())
 }
 
-func updateDomain(auth *bind.TransactOpts, instance *domainRegistry.DomainRegistry, domain string, certificate string) {
-	//FOR NOW
-	//insert a 1 day validity
-	date := time.Now().Add(time.Duration(time.Now().Day())).Unix()
+// updateDomain updates an existing domain in the domain registry contract.
+// It takes the transaction options, the contract instance, the domain name,
+// and the certificate string as input. It sends the transaction and logs the result.
+//
+// Parameters:
+//   - auth: *bind.TransactOpts representing the transaction options.
+//   - instance: *domainRegistry.DomainRegistry instance representing the contract.
+//   - domain: The domain name as a string.
+//   - certificate: The certificate as a PEM-encoded string.
+func updateDomain(auth *bind.TransactOpts, instance *domainRegistry.DomainRegistry, domain string, certificate string, expirationTimestamp int64) {
 
-	tx, err := instance.UpdateDomain(auth, domain, util.GetIP(), certificate, big.NewInt(date))
+	tx, err := instance.UpdateDomain(auth, domain, util.GetIP(), certificate, big.NewInt(expirationTimestamp))
 	if err != nil {
 		log.Fatalf("Impossibile aggiornare il dominio: %v", err)
 	}
 	fmt.Printf("Domain updated: %s", tx.Hash().Hex())
 }
 
+// deleteDomain revokes a domain in the domain registry contract.
+// It takes the transaction options and the contract instance as input,
+// and sends the transaction to revoke the specified domain. It logs the result.
+//
+// Parameters:
+//   - auth: *bind.TransactOpts representing the transaction options.
+//   - instance: *domainRegistry.DomainRegistry instance representing the contract.
+//   - domain: The domain name as a string.
 func deleteDomain(auth *bind.TransactOpts, instance *domainRegistry.DomainRegistry, domain string) {
 	tx, err := instance.DeleteDomain(auth, domain)
 	if err != nil {
@@ -82,6 +112,19 @@ func deleteDomain(auth *bind.TransactOpts, instance *domainRegistry.DomainRegist
 	fmt.Printf("Domain revoked: %s", tx.Hash().Hex())
 }
 
+// getCertificate retrieves the certificate for a domain from the domain registry contract.
+// It takes the transaction options, the contract instance, and the domain name as input.
+// It returns the IP address, the certificate as a PEM-encoded string, and an error if any occurs.
+//
+// Parameters:
+//   - auth: *bind.TransactOpts representing the transaction options.
+//   - instance: *domainRegistry.DomainRegistry instance representing the contract.
+//   - domain: The domain name as a string.
+//
+// Returns:
+//   - ip: The IP address of the domain as a string.
+//   - certificate: The certificate of the domain as a PEM-encoded string.
+//   - error: An error, if any occurs during the execution of the function.
 func getCertificate(auth *bind.TransactOpts, instance *domainRegistry.DomainRegistry, domain string) (string, string, error) {
 	ip, certificate, err := instance.GetCertificate(&bind.CallOpts{}, domain)
 	if err != nil {
@@ -90,7 +133,87 @@ func getCertificate(auth *bind.TransactOpts, instance *domainRegistry.DomainRegi
 	return ip, certificate, err
 }
 
-func Request(client *ethclient.Client, choice int, name string, cert string) (string, string, error) {
+// estimateGas estimates the gas required to perform a specific action on the domain registry contract.
+// It takes the transaction options, the Ethereum client, the contract address, the function name,
+// and a variadic list of arguments for the function. It returns the estimated gas as a uint64 and an error, if any occurs.
+//
+// Parameters:
+//   - auth: *bind.TransactOpts representing the transaction options.
+//   - client: *ethclient.Client instance representing the Ethereum client.
+//   - address: common.Address representing the contract address.
+//   - functionName: The name of the contract function to estimate gas for.
+//   - args: A variadic list of arguments for the specified function.
+//
+// Returns:
+//   - uint64: The estimated gas for the specified function call.
+//   - error: An error, if any occurs during the gas estimation process.
+func estimateGas(auth *bind.TransactOpts, client *ethclient.Client, address common.Address, functionName string, args ...interface{}) (uint64, error) {
+	gasAuth := *auth
+	gasAuth.GasLimit = 0
+
+	_, err := domainRegistry.NewDomainRegistry(address, client)
+	if err != nil {
+		return 0, err
+	}
+
+	var gasEstimate uint64
+	var input abi.Method
+
+	contractABI, err := abi.JSON(strings.NewReader(domainRegistry.DomainRegistryABI))
+	if err != nil {
+		return 0, err
+	}
+
+	found := false
+	for name, method := range contractABI.Methods {
+		if name == functionName {
+			input = method
+			found = true
+			break
+		}
+	}
+	if !found {
+		return 0, fmt.Errorf("unknown function name: %s", functionName)
+	}
+
+	encodedArgs, err := input.Inputs.Pack(args...)
+	if err != nil {
+		return 0, err
+	}
+
+	callMsg := ethereum.CallMsg{
+		From: gasAuth.From,
+		To:   &address,
+		Data: append(input.ID, encodedArgs...),
+	}
+	gasEstimate, err = client.EstimateGas(context.Background(), callMsg)
+	if err != nil {
+		return 0, err
+	}
+
+	return gasEstimate, nil
+}
+
+// Request performs various actions on the domain registry contract based on the user's choice.
+// It takes an ethclient.Client, an integer representing the action to perform, the domain name,
+// and the certificate string as input. The actions are as follows:
+// 1 - Create a new domain.
+// 2 - Revoke a domain.
+// 3 - Update an existing domain.
+// 4 - Get the certificate for a domain.
+// The function returns the IP address and certificate of the domain as strings, and an error if any occurs.
+//
+// Parameters:
+//   - client: *ethclient.Client instance to interact with the Ethereum blockchain.
+//   - choice: Integer representing the action to perform (1: Create, 2: Revoke, 3: Update, 4: Get certificate).
+//   - name: The domain name as a string.
+//   - cert: The certificate as a PEM-encoded string.
+//
+// Returns:
+//   - ip: The IP address of the domain as a string.
+//   - certificate: The certificate of the domain as a PEM-encoded string.
+//   - error: An error, if any occurs during the execution of the function.
+func Request(client *ethclient.Client, choice int, name string, cert string, expirationTimestamp int64) (string, string, error) {
 	privateKey, err := ReadOrCreatePrivateKey(keyfile)
 	if err != nil {
 		log.Fatal(err)
@@ -125,8 +248,7 @@ func Request(client *ethclient.Client, choice int, name string, cert string) (st
 	}
 
 	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)      // in wei
-	auth.GasLimit = uint64(3000000) // in units
+	auth.Value = big.NewInt(0) // in wei
 	auth.GasPrice = gasPrice
 
 	address := common.HexToAddress(contractAdress)
@@ -136,11 +258,29 @@ func Request(client *ethclient.Client, choice int, name string, cert string) (st
 	}
 
 	if choice == 1 { //create certificate
-		createDomain(auth, instance, name, cert)
+		estimatedGas, err := estimateGas(auth, client, address, "createDomain", name, util.GetIP(), cert, big.NewInt(expirationTimestamp))
+		if err != nil {
+			log.Fatalf("Failed to estimate gas: %v", err)
+		}
+		auth.GasLimit = estimatedGas // Use the estimated gas limit
+
+		createDomain(auth, instance, name, cert, expirationTimestamp)
 	} else if choice == 2 { //revoke certificate
+		estimatedGas, err := estimateGas(auth, client, address, "deleteDomain", name)
+		if err != nil {
+			log.Fatalf("Failed to estimate gas: %v", err)
+		}
+		auth.GasLimit = estimatedGas // Use the estimated gas limit
+
 		deleteDomain(auth, instance, name)
 	} else if choice == 3 { //update certificate
-		updateDomain(auth, instance, name, cert)
+		estimatedGas, err := estimateGas(auth, client, address, "updateDomain", name, util.GetIP(), cert, big.NewInt(expirationTimestamp))
+		if err != nil {
+			log.Fatalf("Failed to estimate gas: %v", err)
+		}
+		auth.GasLimit = estimatedGas // Use the estimated gas limit
+
+		updateDomain(auth, instance, name, cert, expirationTimestamp)
 	} else if choice == 4 { //get certificate
 		ip, certificate, err := getCertificate(auth, instance, name)
 		if err != nil {
